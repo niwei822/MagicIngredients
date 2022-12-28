ad3e0a8ca4fe28bc47adf694a586679d7bc5387f
niwei822 <cecily.li.822@gmail.com>
2022-12-27 21:55:59 -0800

    update add favorite

---
+++ static/js/favorite.js
@@ -0,0 +1,10 @@
+var btnvar1 = document.getElementById('heart');
+
+function Toggle() {
+    if (btnvar1.style.color == "red") {
+        btnvar1.style.color = "grey";
+    }
+    else {
+        btnvar1.style.color = "red";
+    }
+}
